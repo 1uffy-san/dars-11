466c2d78531f7b3abeb75792e175c84968960a65
umar <umarzokirov911@gmail.com>
2026-04-03 14:42:45 +0500

    fwahh

---
+++ sf.js
@@ -0,0 +1,13 @@
+
+let form = document.getElementById("form");
+form.addEventListener("keyup" , function(event){
+    console.log(form.elements);
+    console.log(form.elements[0]);
+    console.log(form.elements[0].value);
+    console.log(form.elements[0].value.length);
+    
+    
+    
+    
+}
+)
